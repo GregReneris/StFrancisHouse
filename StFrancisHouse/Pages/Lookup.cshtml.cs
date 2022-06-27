38f3ab3397ae05ed93b4545cc847e5740fccfc91
Kruta-Prabhu <krutaprabhu96@gmail.com>
2022-06-26 23:06:40 -0700

    Added a Lookup page to the project and added a print reciept button to it

---
+++ StFrancisHouse/Pages/Lookup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StFrancisHouse.Pages
+{
+    public class LookupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
