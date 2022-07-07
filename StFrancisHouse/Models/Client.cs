ccb99728f56405b8edcaa68d6b37bfd43c60b261
troy8chen <troy8chen@gmail.com>
2022-07-07 15:55:10 -0700

    Adding ClientID class  in Client.cs

---
+++ StFrancisHouse/Models/Client.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace StFrancisHouse.Models
+{
+    public class Client
+    {
+        public string ClientID { get; set; } = default!;
+
+
+    }
+}
