a30cf3daa887fd0f7f9ebe9463773099dd9da087
troy8chen <troy8chen@gmail.com>
2022-07-07 15:58:49 -0700

    Added field classes for Client.cs

--- StFrancisHouse/Models/Client.cs
+++ StFrancisHouse/Models/Client.cs
@@ -6,6 +6,24 @@ namespace StFrancisHouse.Models
     {
         public string ClientID { get; set; } = default!;
 
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleInitial { get; set; }
+
+        public string Birthday { get; set; }
+
+        public int ZipCode { get; set; }
+
+        public string Race { get; set; }
+
+        public string Gender 
+
+
+
+
+
 
     }
 }
