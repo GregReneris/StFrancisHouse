2ba480f2b03efdb391d9b6656e034feea867f552
troy8chen <troy8chen@gmail.com>
2022-07-07 16:03:51 -0700

    Added classes in Family.cs

--- StFrancisHouse/Models/Family.cs
+++ StFrancisHouse/Models/Family.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace StFrancisHouse.Models
 {
     public class Family
     {
-        public Family()
-        {
-        }
+        public int ID { get; set; }
+
+        public int NumKids { get; set; }
+
+        public int BoyAge { get; set; }
+
+        public int GirlAge { get; set; }
+
     }
 }
 
