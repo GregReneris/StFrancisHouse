fc78aa7893c72c36a068c7fff86ea492beccc556
greg reneris <greg@reneris.com>
2022-07-07 16:05:03 -0700

    adding base framework to JsonFileClient framework

--- StFrancisHouse/Services/JsonFileClientService.cs
+++ StFrancisHouse/Services/JsonFileClientService.cs
@@ -1,4 +1,14 @@
-﻿namespace StFrancisHouse.Services
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Net.Http;
+using System.Net;
+using System;
+
+namespace StFrancisHouse.Services
+
+
 {
     public class JsonFileClientService
     {
