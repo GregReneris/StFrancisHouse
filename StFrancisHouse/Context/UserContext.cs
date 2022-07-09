f4a747f344c54a684bdf7f952b5c4690673120a6
Atlas238 <jack@bentsen.us>
2022-07-09 16:28:56 -0700

    Adding MySql nuget packages + basic files (still working on getting real data)

---
+++ StFrancisHouse/Context/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace StFrancisHouse.Models
+{
+    public class UserContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=stfrancis;user=root;password=root238!");
+        }
+        public UserContext(DbContextOptions<UserContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<User> UserItems { get; set; } = null!;
+    }
+}
