d042aefc3b7c400a6b504586dbe7122f94712ced
greg reneris <greg@reneris.com>
2022-07-17 17:01:05 -0700

    Modifed UserContext.cs to connect to database with connection string call.

--- StFrancisHouse/Context/UserContext.cs
+++ StFrancisHouse/Context/UserContext.cs
@@ -1,11 +1,65 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MySql.Data.MySqlClient;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics.CodeAnalysis;
+using System;
+using System.Collections.Generic;
 
 namespace StFrancisHouse.Models
 {
     public class UserContext : DbContext
     {
 
+        //Adding in mySQL connection string to the online database.
+        public string ConnectionString { get; set; }
+
+        public UserContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+
+        //example method to get all Clients from Client Table on DB.
+        //Note: May be out of date to modified/newest DB Schema based on sqlver1.0, no updates. 
+        //Note: possible null assignments for fields. 
+        public List<Client> getAllClients()
+        {
+            List<Client> clients = new List<Client>();
+            int numEntry = 10; //change this to user chosen value in production later.  
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from Client where id < " + numEntry , conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        clients.Add(new Client()
+                        {
+                            ClientID = Convert.ToInt32(reader["ClientID"]),
+                            FirstName = reader["FirstName"].ToString(),
+                            LastName = reader["LastName"].ToString(),
+                            MiddleInitial = reader["MiddleInitial"].ToString(),
+                            Birthday = reader["Birthday"].ToString(),
+                            ZipCode = Convert.ToInt32(reader[ZipCode]),
+                            Race = reader["Race"].ToString(),
+                            Gender = reader["Gender"].ToString()
+                        });
+                    }
+                }
+
+            }
+
+            return clients; //returns the client list.
+        }
+       
+        //Greg: Not sure if the below needs to be commented out since conectionstring above has been used. 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySQL("server=localhost;database=stfrancis;user=root;password=root238!");
