8d143d63ce0fda51f3ed0cdcd920ca1309575bc6
greg reneris <greg@reneris.com>
2022-07-17 17:11:43 -0700

    Adding dataController and calling getAllClients().

---
+++ StFrancisHouse/Controllers/dataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StFrancisHouse.Models;
+
+namespace StFrancisHouse
+{
+    public class dataController : Controller
+    {
+        
+        //currently calls getAllClients
+        public IActionResult Index()
+        {
+            UserContext context = HttpContext.RequestServices.GetService(typeof(StFrancisHouse.Models.UserContext)) as UserContext;
+
+            return View(context.getAllClients());
+        }
+    }
+}
