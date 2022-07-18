c44e93637113c7f0d9c1f4da64e314ea190f1670
greg reneris <greg@reneris.com>
2022-07-17 21:04:52 -0700

    Adding c# aspnet console log to see action on click.

--- StFrancisHouse/Pages/Lookup.cshtml.cs
+++ StFrancisHouse/Pages/Lookup.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StFrancisHouse.Models;
 
 namespace StFrancisHouse.Pages
 {
@@ -7,6 +8,33 @@ namespace StFrancisHouse.Pages
     {
         public void OnGet()
         {
+            //Console.WriteLine("GO TO LOOKUP PAGE");
         }
+
+        public void getMessage()
+        {
+            Console.WriteLine("WE GOT HERE TO THIS BUTTON");
+
+
+        }
+
     }
 }
+
+
+
+
+
+/// <summary>
+/// REST Get request
+/// </summary>
+/// <param name="id"></param>
+//public void OnGet(string id)
+//{
+//    Hike = HikeService.GetHikes().FirstOrDefault(m => m.Id.Equals(id));
+//    if (Hike == null)
+//    {
+//        throw new ArgumentNullException("ID NOT FOUND");
+//    }
+//
+//}
