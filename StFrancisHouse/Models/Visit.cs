511a48c43bae889fb692a5157ea9c63c33eff5cd
greg reneris <greg@reneris.com>
2022-07-23 22:25:31 -0700

    adding a visit model to track the visit information

---
+++ StFrancisHouse/Models/Visit.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StFrancisHouse.Models
+{
+
+    /// <summary>
+    /// Visit is the model that holds the information displayed in the visit
+    /// It contains relevant dates as well as requests.
+    /// </summary>
+    public class Visit
+    {
+        public int VisitID { get; set; }
+        
+        public string VisitDate { get; set; }
+
+        public string LastBackpage { get; set; }
+
+        public string LastSleepingBag { get; set; }
+
+        public string Requests { get; set; }
+
+
+    }
+}
